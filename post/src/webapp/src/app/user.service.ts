38ec871ca4da2bcbb29dcf90d2fbee7316e8023e
NguyenDuyKhanh <enzo29042003@gmail.com>
2025-03-25 10:54:59 +0700

    merge

---
+++ post/src/webapp/src/app/user.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private baseURL = "http://localhost:8082/api/v1/posts";
+
+  constructor(private http: HttpClient,
+              private router: Router) {
+  }
+
+  getNewFeed(request: any): Observable<any> {
+    return this.http.get(`${this.baseURL}`, request);
+  }
+
+  getCmtByIDPost(id: any) {
+    return this.http.get(`${this.baseURL}/${id}/comments`);
+  }
+
+  upPost(content: any) {
+    return this.http.post<any>(`${this.baseURL}`, content);
+  }
+}
