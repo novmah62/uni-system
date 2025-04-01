d176a534c40efd2e744863c03314f4ada0c639c6
NguyenDuyKhanh <enzo29042003@gmail.com>
2025-04-01 17:02:18 +0700

    merge

--- post/src/webapp/src/app/profile/profile.component.ts
+++ post/src/webapp/src/app/profile/profile.component.ts
@@ -6,13 +6,15 @@ import {MessageService} from 'primeng/api';
 import {CookieService} from 'ngx-cookie-service';
 import {FormsModule} from '@angular/forms';
 import {NgForOf, NgIf} from '@angular/common';
+import {ToastModule} from 'primeng/toast';
 
 @Component({
   selector: 'app-profile',
   imports: [
     FormsModule,
     NgForOf,
-    NgIf
+    NgIf,
+    ToastModule
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
@@ -23,35 +25,44 @@ export class ProfileComponent implements OnInit {
   posts: any = []
   isPopupOpen = false;
   newComment = '';
-  comments: string[] = [];
+  comments: any[] = [];
   selectedPost: any = null;
+  createCommentRequest: any = {}
+  cuUser: any = {};
+
   constructor(private userService: UserService,
               private router: Router,
               private cookieService: CookieService,
               private messageService: MessageService,
               private spinner: NgxSpinnerService) {
   }
-  upPost(){
+
+  upPost() {
     this.userService.upPost(this.createPostRequest).subscribe(res => {
       console.log(res)
     })
   }
 
   ngOnInit(): void {
-    this.userService.getNewFeed(this.request).subscribe(res =>{
+    this.showSuccess("aaa")
+    this.userService.getNewFeed(this.request).subscribe(res => {
       this.posts = res.data.items;
       this.posts.forEach((item: { userInfo: { firstName: any; }; }) => {
         console.log(item.userInfo.firstName)
       })
       console.log(this.posts)
     })
-
+    // this.userService.auth().subscribe(res => {
+    //   this.cuUser = res.data;
+    // })
   }
-  getCmtByPost(id:any){
+
+  getCmtByPost(id: any) {
     this.userService.getCmtByIDPost(id).subscribe(res => {
       console.log(res)
     })
   }
+
   timeAgo(postTime: string | Date) {
     const now = new Date();
 
@@ -77,10 +88,14 @@ export class ProfileComponent implements OnInit {
     if (diffMonths < 12) return `${diffMonths} tháng trước`;
     return `${diffYears} năm trước`;
   }
+
   openPopup(post: any) {
     this.selectedPost = post;
     this.comments = [];
     this.isPopupOpen = true;
+    this.userService.getCmtByIDPost(post.postId).subscribe(res => {
+      this.comments = res.data.items;
+    })
   }
 
   closePopup() {
@@ -88,11 +103,27 @@ export class ProfileComponent implements OnInit {
     this.selectedPost = null;
   }
 
-  addComment() {
-    if (this.newComment.trim()) {
-      this.comments.push(this.newComment);
-      this.selectedPost.comments.push(this.newComment);
-      this.newComment = '';
+  addComment(post: any) {
+    if (this.createCommentRequest.content != null && this.createCommentRequest.content.trim()) {
+      this.createCommentRequest.postId = post.postId
+      this.userService.addComt(this.createCommentRequest).subscribe(res => {
+        if (res.data) {
+          this.showSuccess("Bình luận thành công")
+          this.closePopup();
+        }
+        else {
+          this.showFail("Bình luận thất bại")
+        }
+
+      })
     }
+  }
+  showSuccess(content: any){
+    this.messageService.add({severity: 'success', summary: 'Thành công', detail: content});
+
+  }
+  showFail(content: any) {
+    this.messageService.add({severity: 'error', summary: 'Thành công', detail: content});
+
   }
 }
--- post/src/webapp/src/app/user.service.ts
+++ post/src/webapp/src/app/user.service.ts
@@ -8,24 +8,31 @@ import {Observable} from 'rxjs';
 })
 export class UserService {
   private baseURL = "http://localhost:8082/api/v1/posts";
-  private baseURL1 = "http://localhost:8082/api/v1/users";
+  private baseURL1 = "http://localhost:8081/api/v1/users";
+  private baseURL2 = "http://localhost:8082/api/v1/comments"
+
   constructor(private http: HttpClient,
               private router: Router,
-              ) {
+  ) {
   }
 
   getNewFeed(request: any): Observable<any> {
     return this.http.get(`${this.baseURL}`, request);
   }
 
-  getCmtByIDPost(id: any) {
+  getCmtByIDPost(id: any): Observable<any> {
     return this.http.get(`${this.baseURL}/${id}/comments`);
   }
 
   upPost(content: any) {
     return this.http.post<any>(`${this.baseURL}`, content);
   }
-  auth(){
+
+  auth(): Observable<any>  {
     return this.http.get(`${this.baseURL1}`);
+  }
+
+  addComt(createCommentRequest: any) {
+    return this.http.post<any>(`${this.baseURL2}`, createCommentRequest);
   }
 }
