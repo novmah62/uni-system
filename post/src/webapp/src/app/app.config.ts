80b60fb5fd65ff071b16b20bde11b31c722ba978
novmah62 <at180532@actvn.edu.vn>
2025-04-02 09:16:53 +0700

    config(user-central): update config socket

--- post/src/webapp/src/app/app.config.ts
+++ post/src/webapp/src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),MessageService,
+    MessageService,
     provideHttpClient(
       withInterceptors([keycloakHttpInterceptor])
     ),
